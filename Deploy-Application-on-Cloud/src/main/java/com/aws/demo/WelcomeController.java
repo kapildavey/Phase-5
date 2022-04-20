b9058baf7f2d1ec697f29c3d8129f58403291664
kapildavey <99812570+kapildavey@users.noreply.github.com>
2022-04-20 14:48:28 +0530

    Add files via upload

---
+++ Deploy-Application-on-Cloud/src/main/java/com/aws/demo/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.aws.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Deplying Spring Boot Application on AWS";
+
+	}
+
+}
